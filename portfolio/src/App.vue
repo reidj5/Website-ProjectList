4c7b6237c2e7603cc1342750f1e8efcac348f8bc
Jamie <reidj5@hawkmail.newpaltz.edu>
2021-07-11 13:28:18 -0400

    Initial Vue Setup for Portfolio

---
+++ portfolio/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <Nav/>
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import Nav from './components/Nav.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Nav
+  }
+}
+</script>
+
+<style>
+
+</style>
